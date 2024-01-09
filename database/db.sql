b088492c43aa79b68b9f4c83245aad73e7c689ec
santi <danny.cajamarca8695@utc.edu.ec>
2024-01-09 10:39:12 -0500

    first commit

---
+++ database/db.sql
@@ -0,0 +1,12 @@
+
+CREATE DATABASE customersdb;
+
+use customersdb;
+
+-- crear table
+CREATE TABLE customer (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  address VARCHAR(100) NOT NULL,
+  phone VARCHAR(15)
+);
